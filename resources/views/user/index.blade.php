1b9d73c2b202c3bf9c128e009b50d88f22b2a704
KillPatrick <g.brusokas@gmail.com>
2021-02-09 17:40:43 +0200

    User profile view password errors added

--- resources/views/user/index.blade.php
+++ resources/views/user/index.blade.php
@@ -7,6 +7,7 @@
                 Personal information
             </div>
             <div class="col-md-10">
+
                 <div class="card">
                     <div class="card-body">
                         <form method="POST" action="{{ route('user-profile-information.update') }}">
@@ -15,22 +16,11 @@
                             <div class="form-group row">
                                 <div class="col-md-6">
                                     <label for="name" class="text-md-right">{{ __('Name') }}</label>
-                                    <input id="name" type="text" class="form-control @error('name') is-invalid @enderror" name="name" value="{{ Auth::user()->name }}" required autocomplete="name">
-                                    @error('name')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                    @enderror
+                                    <input id="name" type="text" class="form-control" name="name" value="{{ Auth::user()->name }}" required autocomplete="name">
                                 </div>
                                 <div class="col-md-6">
                                     <label for="email" class="text-md-right">{{ __('E-Mail Address') }}</label>
-                                    <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ Auth::user()->email }}" required autocomplete="email">
-
-                                    @error('email')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                    @enderror
+                                    <input id="email" type="email" class="form-control" name="email" value="{{ Auth::user()->email }}" required autocomplete="email">
                                 </div>
                             </div>
                             <div class="form-group row mb-0">
@@ -58,8 +48,8 @@
                             <div class="form-group row">
                                 <div class="col-md-6">
                                     <label for="current_password" class="text-md-right">{{ __('Current password') }}</label>
-                                    <input id="current_password" type="password" class="form-control @error('current_password') is-invalid @enderror" name="current_password" value="" required>
-                                    @error('current_password')
+                                    <input id="current_password" type="password" class="form-control @error('current_password', 'updatePassword') is-invalid @enderror" name="current_password" value="" required autocomplete="new-password">
+                                    @error('current_password', 'updatePassword')
                                     <span class="invalid-feedback" role="alert">
                                         <strong>{{ $message }}</strong>
                                     </span>
@@ -69,8 +59,8 @@
                             <div class="form-group row">
                                 <div class="col-md-6">
                                     <label for="password" class="text-md-right">{{ __('Password') }}</label>
-                                    <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" value="" required>
-                                    @error('password')
+                                    <input id="password" type="password" class="form-control @error('password', 'updatePassword') is-invalid @enderror" name="password" value="" required autocomplete="new-password">
+                                    @error('password', 'updatePassword')
                                     <span class="invalid-feedback" role="alert">
                                         <strong>{{ $message }}</strong>
                                     </span>
@@ -78,7 +68,7 @@
                                 </div>
                                 <div class="col-md-6">
                                     <label for="password_confirmation" class="text-md-right">{{ __('Confirm Password') }}</label>
-                                    <input id="password_confirmation" type="password" class="form-control @error('password_confirmation') is-invalid @enderror" name="password_confirmation" value="" required>
+                                    <input id="password_confirmation" type="password" class="form-control" name="password_confirmation" value="" required autocomplete="new-password">
                                 </div>
                             </div>
                             <div class="form-group row mb-0">
