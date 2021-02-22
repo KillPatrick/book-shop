753d004a27bdd5ef38de9585f3fb5c0ec8e95985
KillPatrick <g.brusokas@gmail.com>
2021-02-22 16:56:23 +0200

    Review seeder updated

--- database/seeders/ReviewSeeder.php
+++ database/seeders/ReviewSeeder.php
@@ -16,19 +16,20 @@ class ReviewSeeder extends Seeder
      */
     public function run()
     {
-
+        $users = User::all();
+        $books = Book::all();
         for($i = 1; $i <= 500; $i++){
-            $user = User::inRandomOrder()->first();
-
-            $userReviewBookIds = array();
-            foreach($user->reviews as $review){
-                $userReviewBookIds[] =  $review->book_id;
+            $reviewCreated = true;
+            while($reviewCreated){
+                try {
+                    $user = $users->find(rand(1, 20));
+                    $book = $books->find(rand(1, 50));
+                    Review::factory()->create(['user_id' => $user->id, 'book_id' => $book->id]);
+                    $reviewCreated = false;
+                } catch(\Exception $e){
+                    null;
+                }
             }
-
-            $book = Book::inRandomOrder()->whereNotIn('id', $userReviewBookIds)->first();
-
-            Review::factory()->create(['user_id' => $user->id, 'book_id' => $book->id]);
         }
-        //Review::factory()->times(100)->create();
     }
 }
